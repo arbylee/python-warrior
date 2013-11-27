63b21ba8bbb02920fe784d4a83d7969ebfdd9db4
Richard Lee <rblee88@gmail.com>
2013-11-27 13:22:30 -0600

    Add WIP runner and main script

---
+++ pythonwarrior/main.py
@@ -0,0 +1,10 @@
+import runner
+import sys
+
+
+def main():
+    the_runner = runner.Runner([], sys.stdin, sys.stdout)
+    the_runner.run()
+
+if __name__ == "__main__":
+    main()
---
+++ pythonwarrior/runner.py
@@ -0,0 +1,23 @@
+from optparse import OptionParser
+from pythonwarrior.config import Config
+from pythonwarrior.game import Game
+
+
+class Runner(object):
+    def __init__(self, arguments, stdin, stdout):
+        self.arguments = arguments
+        self.stdin = stdin
+        self.stdout = stdout
+        self.game = Game()
+
+    def run(self):
+        Config.in_stream = self.stdin
+        Config.out_stream = self.stdout
+        Config.delay = 0.6
+        self.parse_options()
+        self.game.start()
+
+    def parse_options(self):
+        #TODO FINISH ME!!!
+        options = OptionParser()
+        return options
