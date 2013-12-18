93ccf0db369ff6115851051f1fbc7c88b75dc3d0
Richard Lee <rblee88@gmail.com>
2013-12-17 23:04:50 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pythonwarrior',
+    version='0.0.1',
+    packages=find_packages(),
+    scripts=['bin/pythonwarrior'],
+    author="Richard Lee",
+    author_email="rblee88@gmail.com",
+    description=("Game written in Python for learning Python and AI - "
+                 "a Python port of ruby-warrior"),
+    long_description=open('README.md').read(),
+    install_requires=['jinja2'],
+    license="MIT",
+    url="https://github.com/arbylee/python-warrior",
+    keywords="python ruby warrior rubywarrior pythonwarrior",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha"
+    ]
+)
