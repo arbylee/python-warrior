5a9bdb03842f371bfda514f06e233e4ae6323ec7
Richard Lee <rblee88@gmail.com>
2013-12-20 17:31:07 -0600

    Fix listen ability return Space objects

--- pythonwarrior/abilities/listen.py
+++ pythonwarrior/abilities/listen.py
@@ -6,10 +6,12 @@ class Listen(AbilityBase):
         return "Returns an array of all spaces which have units in them."
 
     def perform(self):
-        def _filter_out_main(item):
-            return (item and item != self._unit)
+        def _collect_non_warrior_spaces(unit):
+            if unit and unit != self._unit:
+                return unit.position.space()
 
-        units = filter(_filter_out_main, self._unit.position.floor.units)
-
+        units = map(_collect_non_warrior_spaces,
+                    self._unit.position.floor.units)
         units = filter(lambda x: x, units)
+
         return units
