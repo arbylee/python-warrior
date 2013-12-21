49169a5de4ff7a944233bf5739be0326bbf06ad3
Richard Lee <rblee88@gmail.com>
2013-12-21 10:12:30 -0600

    Add detonate ability

---
+++ pythonwarrior/abilities/detonate.py
@@ -0,0 +1,28 @@
+from pythonwarrior.abilities.base import AbilityBase
+
+
+class Detonate(AbilityBase):
+    def description(self):
+        return ("Detonate a bomb in a given direction (forward by default) "
+                "which damages that space and surrounding 4 spaces "
+                "(including yourself).")
+
+    def perform(self, direction='forward'):
+        self.verify_direction(direction)
+        if self._unit.position:
+            self._unit.say("detonates a bomb %s launching "
+                           "a deadly explosion" % direction)
+            self.bomb(direction, 1, 0, 8)
+            for x, y in [(1, 1), (1, -1), (2, 0), (0, 0)]:
+                self.bomb(direction, x, y, 4)
+
+    def bomb(self, direction, x, y, damage_amount):
+        if self._unit.position:
+            receiver = self.space(direction, x, y).unit
+            if receiver:
+                if receiver.abilities.get('explode'):
+                    receiver.say("caught in bomb's flames which "
+                                 "detonates ticking explosive")
+                    receiver.abilities['explode'].perform()
+                else:
+                    self.damage(receiver, damage_amount)
---
+++ pythonwarrior/tests/abilities/test_detonate.py
@@ -0,0 +1,44 @@
+import unittest
+
+from pythonwarrior.abilities.detonate import Detonate
+from pythonwarrior.floor import Floor
+from pythonwarrior.units.base import UnitBase
+from pythonwarrior.units.warrior import Warrior
+
+
+class TestDetonate(unittest.TestCase):
+    def setUp(self):
+        self.floor = Floor()
+        self.floor.width = 4
+        self.floor.height = 3
+        self.warrior = Warrior()
+        self.warrior._health = 20
+        self.detonate = Detonate(self.warrior)
+
+        self.target = UnitBase()
+        self.target._health = 10
+
+        self.floor.add(self.warrior, 0, 1, 'east')
+
+    def test_should_deal_8_damage_if_forward_1_space(self):
+        self.floor.add(self.target, 1, 1)
+        self.detonate.perform()
+        self.assertEqual(2, self.target._health)
+
+    def test_should_deal_4_damage_if_forward_2_spaces(self):
+        self.floor.add(self.target, 2, 1)
+        self.detonate.perform()
+        self.assertEqual(6, self.target._health)
+
+    def test_should_deal_4_damage_if_diagonal_1_space(self):
+        self.floor.add(self.target, 1, 2)
+        self.detonate.perform()
+        self.assertEqual(6, self.target._health)
+
+        self.floor.add(self.target, 1, 0)
+        self.detonate.perform()
+        self.assertEqual(2, self.target._health)
+
+    def test_should_deal_4_damage_to_the_user(self):
+        self.detonate.perform()
+        self.assertEqual(16, self.warrior._health)
